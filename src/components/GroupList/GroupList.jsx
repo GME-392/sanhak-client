bd973d75960076447844ffbd543fc926ed9fdc1d
c17an <kimchanmin1@gmail.com>
2021-03-16 23:18:47 +0900

    fix: 스타일 폴더 분리

---
+++ src/components/GroupList/GroupList.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GroupList = () => {
+  return <div>그룹 리스트</div>;
+};
+
+export default GroupList;
--- src/components/GroupSearch/GroupSearch.jsx
+++ src/components/GroupSearch/GroupSearch.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./GroupSearch.scss";
 
 const GroupSearch = () => {
   return (
