0cd9ef7712cf86f40c5ff71e25c38be082683e9c
c17an <kimchanmin1@gmail.com>
2021-04-03 16:44:39 +0900

    그룹 태그 컴포넌트 제작

--- src/components/CreateGroupModal/CreateGroupModal.jsx
+++ src/components/CreateGroupModal/CreateGroupModal.jsx
@@ -6,13 +6,32 @@ import {
   InputGroup,
   Modal,
 } from "react-bootstrap";
-import React from "react";
+import React, { useState } from "react";
+import Tag from "../Tag/Tag";
 
 const CreateGroupModal = ({
   showCreateGroupModal,
   setShowCreateGroupModal,
 }) => {
   const handleClose = () => setShowCreateGroupModal(false);
+  const [name, setName] = useState(null);
+  const [memberLimit, setMemberLimit] = useState(null);
+  const [tagName, setTagName] = useState(null);
+  const [tagList, setTagList] = useState([]);
+
+  const onPressEnter = (e) => {
+    e.persist();
+    if (e.key === "Enter") {
+      setTagList([...tagList, tagName]);
+      setTagName("");
+    }
+    console.log(tagList);
+  };
+
+  const removeTag = (name) => {
+    console.log(name);
+    setTagList(tagList.filter((tag) => tag !== name));
+  };
 
   return (
     <Modal show={showCreateGroupModal} onHide={handleClose}>
@@ -26,6 +45,8 @@ const CreateGroupModal = ({
             <Form.Control
               type="text"
               placeholder="그룹 이름을 4 ~ 20자 사이로 입력하세요"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
             />
             <Form.Text className="text-muted">
               입력한 그룹명을 통해 사용자들이 검색할 수 있습니다.
@@ -34,7 +55,14 @@ const CreateGroupModal = ({
 
           <Form.Group controlId="groupMemberLimit">
             <Form.Label>최대 그룹원 수</Form.Label>
-            <Form.Control type="number" placeholder="20" min={1} max={40} />
+            <Form.Control
+              type="number"
+              placeholder="20"
+              min={1}
+              max={40}
+              value={memberLimit}
+              onChange={(e) => setMemberLimit(e.target.value)}
+            />
           </Form.Group>
 
           <Form.Group controlId="groupTag">
@@ -42,7 +70,13 @@ const CreateGroupModal = ({
             <Form.Control
               type="text"
               placeholder="DFS, 취업스터디 등 그룹을 나타내는 키워드를 입력하세요"
+              value={tagName}
+              onChange={(e) => setTagName(e.target.value)}
+              onKeyPress={onPressEnter}
             />
+            {tagList.map((tag, idx) => (
+              <Tag name={tag} key={idx} onRemove={removeTag} />
+            ))}
             <Form.Text className="text-muted">
               불건전한 태그를 포함할 경우 경고 없이 태그가 제거될 수 있습니다.
             </Form.Text>
